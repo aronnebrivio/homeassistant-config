70870ac588ba454e65891a15f101c1ad49fb14f5
Aronne Brivio <aronne.brivio@gmail.com>
2023-01-05 17:04:23 +0100

    feat(pyscript): fix shopping list sync with Todoist using async functions for API calls

--- pyscript/apps/shopping_list_sync.py
+++ pyscript/apps/shopping_list_sync.py
@@ -1,67 +1,80 @@
-import time
-
 import requests
+import sys
+import time
+import aiohttp
+import asyncio
 import write_file
 
-todoist_token = pyscript.app_config["todoist_token"]
-todoist_project_id = pyscript.app_config["todoist_project_id"]
+TODOIST_TOKEN = pyscript.app_config["todoist_token"]
+TODOIST_PROJECT_ID = pyscript.app_config["todoist_project_id"]
+
 
 def get_tasks():
-    get_tasks_url =f"https://api.todoist.com/rest/v1/tasks?project_id={todoist_project_id}"
-    headers = {"Authorization" : f"Bearer {todoist_token}"}
+  url = f"https://api.todoist.com/rest/v2/tasks?project_id={TODOIST_PROJECT_ID}"
+  headers = {"Authorization" : f"Bearer {TODOIST_TOKEN}"}
 
-    status_code = 500
+  status_code = 500
 
-    while status_code is not 200:
-        response = task.executor(requests.get, get_tasks_url, headers = headers)
-        status_code = response.status_code
-        time.sleep(1)
-    json = response.json()
-    return json
+  async with aiohttp.ClientSession() as session:
+      async with session.get(url, headers=headers) as response:
+          status_code = response.status
+          #time.sleep(1)
+          json = response.json()
+  return json
+
 
 def add_task(item):
-    url = "https://api.todoist.com/rest/v1/tasks"
-    headers = {"Authorization" : f"Bearer {todoist_token}", "Content-Type" : "application/json"}
-    body = {"content" : item, "project_id" : todoist_project_id}
+    url = "https://api.todoist.com/rest/v2/tasks"
+    headers = {"Authorization" : f"Bearer {TODOIST_TOKEN}", "Content-Type" : "application/json"}
+    body = {"content" : item, "project_id" : TODOIST_PROJECT_ID}
     status_code = 500
 
-    while status_code is not 200:
-        response = task.executor(requests.post, url, headers = headers, json=body)
-        status_code = response.status_code
-        time.sleep(1)
+    async with aiohttp.ClientSession() as session:
+      async with session.post(url, headers = headers, json=body) as response:
+        status_code = response.status
+        #print(await response.text())
+        #json = await response.json()
+        #time.sleep(1)
+
     if status_code == 200:
         return True
     else:
         return False
 
 def update_task(id, content):
-    url = f"https://api.todoist.com/rest/v1/tasks/{id}"
-    headers = {"Authorization" : f"Bearer {todoist_token}", "Content-Type" : "application/json"}
+    url = f"https://api.todoist.com/rest/v2/tasks/{id}"
+    headers = {"Authorization" : f"Bearer {TODOIST_TOKEN}", "Content-Type" : "application/json"}
     body = {"content" : content}
     status_code = 500
 
-    counter = 0
-    while status_code is not 204 and counter < 10:
-        response = task.executor(requests.post, url, headers = headers, json=body)
-        status_code = response.status_code
-        time.sleep(1)
-        counter += 1
+    timeout = aiohttp.ClientTimeout(total=60)
+
+    async with aiohttp.ClientSession(timeout=timeout) as session:
+      async with session.post(url, headers = headers, json=body) as response:
+        status_code = response.status
+        #print(await response.text())
+        #json = await response.json()
+        #time.sleep(1)
+
     if status_code == 204:
         return True
     else:
         return False
 
 def complete_task(id):
-    url = f"https://api.todoist.com/rest/v1/tasks/{id}/close"
-    headers = {"Authorization" : f"Bearer {todoist_token}"}
+    url = f"https://api.todoist.com/rest/v2/tasks/{id}/close"
+    headers = {"Authorization" : f"Bearer {TODOIST_TOKEN}"}
     status_code = 500
 
-    counter = 0
-    while status_code is not 204 and counter < 10:
-        response = task.executor(requests.post, url, headers = headers)
-        status_code = response.status_code
-        time.sleep(1)
-        counter += 1
+    timeout = aiohttp.ClientTimeout(total=60)
+
+    async with aiohttp.ClientSession(timeout=timeout) as session:
+      async with session.post(url, headers = headers) as response:
+        status_code = response.status
+        #print(await response.text())
+        #json = await response.json()
+        #time.sleep(1)
+
     if status_code == 204:
         return True
     else:
@@ -71,10 +84,11 @@ def complete_task(id):
 def sync_shopping_list():
     tasks = []
     json = get_tasks()
+    #await json
     for item in json:
-        tasks.append({"name" : item["content"], "id" : str(item["id"]), "complete" : item["completed"]})
+        tasks.append({"name" : item["content"], "id" : str(item["id"]), "complete" : item["is_completed"]})
 
-    write_file.write_json(filename="/config/.shopping_list.json", content=tasks)
+    task.executor(write_file.write_json, filename = "/config/.shopping_list.json", content=tasks)
     hass.data["shopping_list"].async_load()
 
 @event_trigger('shopping_list_updated')
